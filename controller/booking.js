9186891957c00c78a6e57a350c2a5edd9d245391
shira Zager <mkwnytd@gmail.com>
2021-11-09 10:39:46 +0200

    start booking function

--- controller/booking.js
+++ controller/booking.js
@@ -1,8 +1,21 @@
 var express = require('express')
 var router = express.Router()
 
-router.post('/requestRoom', async (req, res) => {
+router.post('/bookingRequestToServer', async (req, res) => {
     const { date, fromTime, toTime, numberOfParticipants } = req.body
+    try {
+        const findRoom = await Room
+        if (!existingUser) {
+            res.status(400).send("User or Password Invalid")
+            return;
+        }
+
+        res.json({ token: jwt.sign({ email }, process.env.SECRET, { expiresIn: "2h" }) })
+
+    } catch (error) {
+        console.log("Error: ", error)
+        res.status(500).send(error)
+    }
 })
 
 
